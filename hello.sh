116fdfefb681cac8f68828e5f79ea804e4b999d7
jinying888 <wanghe888@live.cn>
2017-08-02 13:45:24 +0800

    add some shellscript

---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo hello,world
+printenv
+echo $PATH
+a='nice to meet you'
+echo $a
---
+++ maxconn.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+for x in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+do
+/usr/local/php/bin/php ~/myprocess/test_client.php
+echo $x
+done
+
+while ((1)) 
+
+do 
+
+/usr/local/php/bin/php ~/myprocess/test_client.php
+
+done;
